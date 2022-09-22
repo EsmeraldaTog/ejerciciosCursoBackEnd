d12705c9716b4fbf699d1d5934b0d82faa6efabc
Esmeralda <esmeraldatorres.2305@gmail.com>
2022-09-22 02:14:13 -0500

    desafio 3 servidor con express

--- archivos.js
+++ archivos.js
@@ -1,182 +1,167 @@
+const fs = require('fs')
 
-const fs = require("fs");
-class Contenedor {
-      constructor(archivo) {
-            this.archivo = archivo;
-      }
-      /* Metodo para  Escribir en el archivo*/
-      async escribirArchivo(archivo, contenido) {
-            try {
-                  await fs.promises.writeFile(archivo,JSON.stringify(contenido, null, 2),"utf-8");
-            } catch (error) {
-                  console.log(error.message);
-            }
-      }
-      /* Metodo para  Leer el archivo*/
-      async leerArchivo(archivo) {
-            try {
-                  const data = await fs.promises.readFile(archivo, "utf-8");
-                  return JSON.parse(data);
-            } catch (error) {
-                  console.log(error.message);
-            }
-      }
-      /* Metodo para  saber si existe el archivo*/
-      saberSiExiste(archivo) {
-            try {
-                  if (!fs.existsSync(archivo)) {
-                        throw new Error("El archivo no se encontro!!");
-                  } else {
-                        return true;
-                  }
-            } catch (error) {
-                  console.log(error.message);
-            }
-      }
+class Contenedor{
+    constructor(file){
+        this.file= file
+    }
 
-      /* Metodo save para guardar un nuevo producto en el archivo*/
-      async save(producto) {
-            try {
-                  /*Primero verifica si existe el archivo, si no exite crea uno nuevo */
-                  if (!this.saberSiExiste(this.archivo)) {
-                        console.log(
-                              `No se encontro el archivo ${this.archivo}\n se procede a crear uno nuevo`
-                        );
-                        /*Se crea un objeto vacio */
-                        let arrayProductos = [];
-                        /*al ser el primer propducto se le da la id 1 */
+
+/* Metodo para  Escribir en el archivo*/
+async escribirArchivo(file, contenido) {
+try {
+      await fs.promises.writeFile(file,JSON.stringify(contenido, null, 2),"utf-8");
+} catch (error) {
+      console.log(error.message);
+}
+}
+/* Metodo para  Leer el archivo*/
+async leerArchivo(file) {
+try {
+      const data = await fs.promises.readFile(file ,"utf-8");
+      return JSON.parse(data);
+} catch (error) {
+      console.log(error.message);
+}
+}
+
+saberSiExiste(file) {
+try {
+      if (!fs.existsSync(file)) {
+            throw new Error("El file no se encontro!!");
+      } else {
+            return true;
+      }
+} 
+catch (error) {
+      console.log(error.message);
+}
+}
+
+
+async save(producto) {
+try {
+      /*Primero verifica si existe el file, si no exite crea uno nuevo */
+      if (!this.saberSiExiste(this.file)) {
+         console.log(`No se encontro el file ${this.file}\n creando file `);
+           //crea arreglo
+            let productos = [];
+            /*al ser el primer propducto se le da la id 1 */
+            producto["id"] = 1;
+          productos.push(producto);
+            console.log("se esta agregando el producto");
+            /* se escribe el file*/
+            await this.escribirArchivo(this.file,productos);
+            console.log( `Se agrego un nuevo producto con la id ${producto["id"]}`);
+            return producto["id"];
+    } 
+      else {
+            /*Si el archivo existe  primero se verifica si esta vacio*/
+            if (this.leerArchivo(this.file)) {
+                  const data = await this.leerArchivo(this.file);
+                  if (data.length === 0) {
+                        /*si esta vacio  se le asigna la id 1 al primer producto*/
                         producto["id"] = 1;
-                        arrayProductos.push(producto);
-                        console.log("se esta agregando el producto");
-                        /* se escribe el archivo*/
-                        await this.escribirArchivo(
-                              this.archivo,
-                              arrayProductos
-                        );
-                        console.log(
-                              `Se agrego un nuevo producto con la id ${producto["id"]}`
-                        );
-                        return producto["id"];
-                  } else {
-                        /*Si el archivo existe  primero se verifica si esta vacio*/
-                        if (this.leerArchivo(this.archivo)) {
-                              const data = await this.leerArchivo(this.archivo);
-                              if (data.length === 0) {
-                                    /*si esta vacio  se le asigna la id 1 al primer producto*/
-                                    producto["id"] = 1;
-                              } else {
-                                    /*Si tiene producto se le asigna la id siguente */
-                                    let ultimoId = data[data.length - 1].id;
-                                    producto["id"] = ultimoId + 1;
-                              }
-                              data.push(producto);
-                              console.log("se esta agregando el producto");
-                              /*se escribe el producto */
-                              this.escribirArchivo(this.archivo, data);
-                              console.log(
-                                    `Se agrego un nuevo producto con la id ${producto["id"]}`
-                              );
-                              return producto["id"];
-                        }
+                  } 
+                  else {
+                        /*Si tiene producto se le asigna la id siguente */
+                        let ultimoId = data[data.length - 1].id;
+                        producto["id"] = ultimoId + 1;
                   }
-            } catch (error) {
-                  console.log(error.message);
+                data.push(producto);
+              console.log("Agregando producto");
+                  /*se escribe el producto */
+                  await this.escribirArchivo(this.file, data);
+                  console.log(`Se agrego un nuevo producto con la id ${producto["id"]}`
+                  );
+                  return producto["id"];
             }
       }
-      /*Metodo getById para obtener un producto por la id*/
-      async getById(id) {
-            try {
-                  /*Primero verifica si existe el archivo, */
-                  if (this.saberSiExiste(this.archivo)) {
-                        const data = await this.leerArchivo(this.archivo);
-                        /* se filtra el archivo para buscar el producto con la id ingresada */
-                        const dataId = data.filter((item) => item.id === id);
-                        if (dataId.length === 0) {
-                              /* si no existe se lanza un error */
-                              throw new Error("No se encontro el ID");
-                        } else {
-                              console.log(
-                                    `El producto con la id ${id} :\n`,
-                                    dataId
-                              );
+} 
+catch (error) {
+      console.log(error.message);
+}
+}
 
-                              return dataId;
-                        }
-                  }
-            } catch (error) {
-                  console.log(error.message);
+async getById(id) {
+try {
+    
+      if (this.saberSiExiste(this.file)) {
+            const data = await this.leerArchivo(this.file);
+            /*  buscar el producto con la id ingresada */
+            const dataId = data.filter(item => item.id === id);
+            if (dataId.length === 0) {
+                  /* si no existe se lanza un error */
+                  throw new Error("No se encontro el ID");
+            } else {
+                  console.log(`El producto con la id ${id} :\n`,dataId);
+                  return dataId;
             }
       }
-      /*Metodo getAll para obtener todos los productos*/
-      async getAll() {
-            try {
-                  /*Primero verifica si existe el archivo, */
-                  if (this.saberSiExiste(this.archivo)) {
-                        console.log("se esta leyendo el archivo");
-                        const data = await this.leerArchivo(this.archivo);
-                        /* se verifica si el archivo esta vacio */
-                        if (data.length !== 0) {
-                              return data;
-                        } else {
-                              /*si esta vacio se lanza un error */
-                              throw new Error(
-                                    `el archivo ${this.archivo} esta vacio`
-                              );
-                        }
-                  }
-            } catch (error) {
-                  console.log(error.message);
+} catch (error) {
+      console.log(error.message);
+}
+}
+
+async getAll() {
+try {
+     
+      if (this.saberSiExiste(this.file)) {
+            console.log(" leyendo el archivo");
+            const data = await this.leerArchivo(this.file);
+            
+            if (data.length !== 0) {
+                //   console.log(`Contenido del archivo ${this.file} :\n`, data);
+                return data
+            }
+             else {
+                  /*si esta vacio se lanza un error */
+                  throw new Error(`el archivo ${this.file} esta vacio`);
             }
       }
-      /*Metodo deleteById para eliminar un producto por la id*/
-      async deleteById(id) {
-            try {
-                  /*Primero verifica si existe el archivo, */
-                  if (this.saberSiExiste(this.archivo)) {
-                        console.log(
-                              `se esta buscando el producto con la id ${id}`
-                        );
-                        const data = await this.leerArchivo(this.archivo);
-                        /* se verifica que la id exista */
-                        if (data.some((item) => item.id === id)) {
-                              const data = await this.leerArchivo(this.archivo);
-                              /*se elima el producto */
-                              const datos = data.filter(
-                                    (item) => item.id !== id
-                              );
-                              this.escribirArchivo(this.archivo, datos);
-                              console.log(
-                                    `se borro el producto con la id ${id}`
-                              );
-                        } else {
-                              /*Si la id no existe se lanza un error */
-                              throw new Error(
-                                    `no se encontro el producto con la id ${id}`
-                              );
-                        }
-                  }
-            } catch (error) {
-                  console.log(error.message);
+} 
+catch (error) {
+      console.log(error.message);
+}
+}
+/* deleteById para eliminar un producto por la id*/
+async deleteById(id) {
+try {
+      /*Primero verifica si existe el archivo, */
+      if (this.saberSiExiste(this.file)) {
+            console.log(`se esta buscando el producto con la id ${id}`);
+            const data = await this.leerArchivo(this.file);
+            /* se verifica que la id exista */
+            if (data.some((item) => item.id === id)) {
+                  const data = await this.leerArchivo(this.file);
+                  /*se elima el producto */
+                  const datos = data.filter(item => item.id !== id);
+                  this.escribirArchivo(this.file, datos);
+                  console.log( `se borro el producto con la id ${id}`);
+            } else {
+                  /*Si la id no existe se lanza un error */
+                  throw new Error(
+                        `no se encontro el producto con la id ${id}`
+                  );
             }
       }
-      /*Metodo deleteAll para eliminar todos los  productos */
-      async deleteAll() {
-            try {
-                  /*Primero verifica si existe el archivo, */
-                  if (this.saberSiExiste(this.archivo)) {
-                        console.log("se esta procediendo a borrar los datos");
-                        /*Para eliminar se escribe un objeto vacio */
-                        let nuevo = [];
-                        await this.escribirArchivo(this.archivo, nuevo);
-                        console.log(
-                              `se borraron todos los datos de el archivo ${this.archivo}`
-                        );
-                  }
-            } catch (error) {
-                  console.log(error.message);
-            }
+} catch (error) {
+      console.log(error.message);
+}
+}
+/*Metodo deleteAll para eliminar todos los  productos */
+async deleteAll() {
+try {
+      /*Primero verifica si existe el archivo, */
+      if (this.saberSiExiste(this.file)) {
+            console.log("se esta procediendo a borrar los datos");
+            /*Para eliminar se escribe un objeto vacio */
+            let nuevo = [];
+            await this.escribirArchivo(this.file, nuevo);
+            console.log(`se borraron todos los datos de el archivo ${this.file}`);
       }
+} catch (error) {
+      console.log(error.message);
+}
+}
 }
 module.exports = Contenedor;
-
-
--- index.js
+++ index.js
@@ -6,8 +6,7 @@ const productos = new Contenedor("productos.txt");
 const app = express();
 const PORT = 8080;
 const server = app.listen(PORT, () => {
-      console.log(
-            `Servidor http escuchando en el puerto ${server.address().port}`
+      console.log(`Servidor http escuchando en el puerto ${server.address().port}`
       );
 });
 server.on("error", (error) => console.log(`Error en servidor ${error}`));
@@ -23,18 +22,11 @@ app.get("/productos", (req, res) => {
     const ejecutar = async () => {
           /*su usa el medo getAll para traer todos los productos */
           const arrayProductos = await productos.getAll();
-          /*se crea un template para mostrar los productos */
-          let template = ``;
-          arrayProductos.map(
-                (item) =>
-                      (template += `<h1 style="color: blue">${item.title} </h1>\n <h2 >Precio $<span style="color: red">${item.price}</span></h2>\n<img width="50px" height="auto"src="${item.thumbnail}">`)
-          );
-          res.send(template);
+      
+          res.send(arrayProductos);
     };
     ejecutar();
 });
-
-
 
 
 
@@ -52,9 +44,8 @@ app.get("/productoRandom", (req, res) => {
             const listaProductos = array.map(
                   (item, index) => index === numero && productoRandom.push(item)
             );
-            /*se crea un template para mostrar el producto */
-            let template = `<h1 style="color: blue">${productoRandom[0].title} </h1>\n <h2 >Precio $<span style="color: red">${productoRandom[0].price}</span></h2>\n<img width="50px" height="auto"src="${productoRandom[0].thumbnail}">`;
-            res.send(template);
+           
+            res.send(productoRandom);
       };
       random();
 });
